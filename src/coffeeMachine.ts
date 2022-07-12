681e4db56f58cbbe8347ffadc79fa714b908eae7
Sidonie Moulart <sidonie.moulart@gmail.com>
2022-07-12 21:28:08 +0200

    Now you have to pay for your coffee

--- src/coffeeMachine.ts
+++ src/coffeeMachine.ts
@@ -6,16 +6,47 @@ export type Choice = {
 }
 interface OrderType {
   order : Choice;
+  price : number;
+  amountMoney : number;
 }
 
 export class Order implements OrderType {
   order: Choice;
 
-  constructor(order: Choice) {
+  price : number;
+
+  amountMoney : number;
+
+  constructor(order: Choice, amountMoney : number) {
     this.order = order;
+    this.price = this.findPrice(this.order.drink);
+    this.amountMoney = amountMoney;
+  }
+
+  findPrice(drink : Drink) {
+    if (drink === 'C') {
+      return 0.6;
+    } if (drink === 'T') {
+      return 0.4;
+    }
+    return 0.5;
   }
 
   orderDisplay() {
-    return `${this.order.drink}:${this.order.sugar ? this.order.sugar : ''}:${this.order.sugar ? 0 : ''}`;
+    let isEnoughMoney = true;
+    switch (this.order.drink) {
+      case 'C':
+        isEnoughMoney = this.amountMoney >= 60;
+        break;
+      case 'T':
+        isEnoughMoney = this.amountMoney >= 40;
+        break;
+      default:
+        isEnoughMoney = this.amountMoney >= 50;
+    }
+    const display = isEnoughMoney
+      ? `${this.order.drink}:${this.order.sugar ? this.order.sugar : ''}:${this.order.sugar ? 0 : ''}`
+      : '';
+    return display;
   }
 }
